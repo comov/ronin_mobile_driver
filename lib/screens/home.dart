6877962d0c1dd72225783d05fb7bd531e2760b51
isafiulin <isafiulin@neotelecom.kg>
2022-08-06 17:16:46 +0600

    Update home.dart with render problem

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -338,114 +338,131 @@ class _HomeState extends State<Home> {
 
           return ListView(
             children: <Widget>[
-              Expanded(
-                child: Card(
-                    child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const SizedBox(height: 5),
-                        Text(
-                          "Фамилия: ${profile?.lastName}",
-                          textAlign: TextAlign.justify,
-                        ),
-                        Text(
-                          "Имя: ${profile?.firstName}",
-                          textAlign: TextAlign.start,
-                        ),
-                        Text(
-                          "Номер телефона: ${profile?.phone}",
-                        ),
-                        const SizedBox(height: 5),
-                        TextButton(
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Expanded(
+                    child: Card(
+                        child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const SizedBox(height: 5),
+                            Text(
+                              "Фамилия: ${profile?.lastName}",
+                              textAlign: TextAlign.justify,
+                            ),
+                            Text(
+                              "Имя: ${profile?.firstName}",
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              "Номер телефона: ${profile?.phone}",
+                            ),
+                            const SizedBox(height: 5),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(
+                                    context,
+                                    "/user/user_edit",
+                                    arguments: UserEditArs(profile: profile),
+                                  );
+                                },
+                                child: const Text("Редактировать профиль"))
+                          ]),
+                    )),
+                  ),
+                ],
+              ),
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Expanded(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      // padding: const EdgeInsets.all(1),
+                      itemCount: carList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          child: ExpansionTileCard(
+                            title: Text(
+                                '${carList[index].brand} ${carList[index].model}'),
+                            subtitle: Text(carList[index].plateNumber),
+                            children: <Widget>[
+                              const Divider(
+                                thickness: 1.0,
+                                height: 1.0,
+                              ),
+                              Align(
+                                  alignment: Alignment.center,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      // horizontal: 16.0,
+                                      // vertical: 8.0,
+                                    ),
+                                    child: Column(
+                                      children: <Widget>[
+                                        const SizedBox(height: 5),
+                                        Text(
+                                            "id: ${carList[index].id.toString()}"),
+                                        Text(
+                                            "Марка авто: ${carList[index].brand}"),
+                                        Text(
+                                            "Модель авто: ${carList[index].model}"),
+                                        Text(
+                                            "Гос. Номер: ${carList[index].plateNumber}"),
+                                        Text("VIN авто: ${carList[index].vin}"),
+                                        Text(
+                                            "Год авто: ${carList[index].year.toString()}"),
+                                        // const SizedBox(height: 5),
+                                      ],
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) =>
+                          const Divider(),
+                    ),
+                  ),
+                ],
+              ),
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Expanded(
+                    child: Card(
+                      child: Column(
+                        children: <Widget>[
+                          // todo: Need to delete phoneNumber, authToken, refreshKey from this page
+                          const Text("##### Хранилище приложения #####"),
+                          const Divider(),
+                          Text("phoneNumber: $phoneNumber"),
+                          const Divider(),
+                          Text("authToken: $authToken"),
+                          const Divider(),
+                          Text("refreshKey: $refreshKey"),
+                          const Divider(),
+
+                          const Text("##### Кнопки #####"),
+                          Row(),
+                          ElevatedButton(
                             onPressed: () {
-                              Navigator.pushNamed(
-                                context,
-                                "/user/user_edit",
-                                arguments: UserEditArs(profile: profile),
+                              delFromStorage();
+                              Navigator.of(context).pushNamedAndRemoveUntil(
+                                "/signin",
+                                (route) => false,
                               );
                             },
-                            child: const Text("Редактировать профиль"))
-                      ]),
-                )),
-              ),
-              Expanded(
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  // padding: const EdgeInsets.all(1),
-                  itemCount: carList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      child: ExpansionTileCard(
-                        title: Text(
-                            '${carList[index].brand} ${carList[index].model}'),
-                        subtitle: Text(carList[index].plateNumber),
-                        children: <Widget>[
-                          const Divider(
-                            thickness: 1.0,
-                            height: 1.0,
+                            child: const Text("Выйти"),
                           ),
-                          Align(
-                              alignment: Alignment.center,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0,
-                                  vertical: 8.0,
-                                ),
-                                child: Column(
-                                  children: <Widget>[
-                                    const SizedBox(height: 5),
-                                    Text("id: ${carList[index].id.toString()}"),
-                                    Text("Марка авто: ${carList[index].brand}"),
-                                    Text(
-                                        "Модель авто: ${carList[index].model}"),
-                                    Text(
-                                        "Гос. Номер: ${carList[index].plateNumber}"),
-                                    Text("VIN авто: ${carList[index].vin}"),
-                                    Text(
-                                        "Год авто: ${carList[index].year.toString()}"),
-                                    const SizedBox(height: 5),
-                                  ],
-                                ),
-                              ))
                         ],
                       ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                ),
-              ),
-              Expanded(
-                child: Card(
-                  child: Column(
-                    children: <Widget>[
-                      // todo: Need to delete phoneNumber, authToken, refreshKey from this page
-                      const Text("##### Хранилище приложения #####"),
-                      const Divider(),
-                      Text("phoneNumber: $phoneNumber"),
-                      const Divider(),
-                      Text("authToken: $authToken"),
-                      const Divider(),
-                      Text("refreshKey: $refreshKey"),
-                      const Divider(),
-
-                      const Text("##### Кнопки #####"),
-                      Row(),
-                      ElevatedButton(
-                        onPressed: () {
-                          delFromStorage();
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                            "/signin",
-                            (route) => false,
-                          );
-                        },
-                        child: const Text("Выйти"),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ],
           );
