063ddacc8bd2c1feea5ae5cc08f0662b2d42554d
isafiulin <isafiulin@neotelecom.kg>
2022-08-10 18:39:44 +0600

    Refactored design of the page CreateCar

--- lib/screens/user/create_car.dart
+++ lib/screens/user/create_car.dart
@@ -12,17 +12,15 @@ class CreateCar extends StatefulWidget {
   State<CreateCar> createState() => _CreateCarState();
 }
 
-class _CreateCarState  extends State<CreateCar> {
+class _CreateCarState extends State<CreateCar> {
   String authToken = "";
   List<Car> carList = [];
 
-  String  brand = "";
-  String  model = "";
-  int?     year;
-  String  vin = "";
-  String  plateNumber = "";
-
-
+  String brand = "";
+  String model = "";
+  int? year;
+  String vin = "";
+  String plateNumber = "";
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +31,13 @@ class _CreateCarState  extends State<CreateCar> {
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         return Scaffold(
           appBar: AppBar(
-            // title: const Text("Добавление авто"),
-            // titleTextStyle: const TextStyle(
-            //   color: Colors.black,
-            //   fontWeight: FontWeight.bold,
-            //   fontSize: 20,
-            // ),
-          ),
+              // title: const Text("Добавление авто"),
+              // titleTextStyle: const TextStyle(
+              //   color: Colors.black,
+              //   fontWeight: FontWeight.bold,
+              //   fontSize: 20,
+              // ),
+              ),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: ListView(
@@ -49,148 +47,143 @@ class _CreateCarState  extends State<CreateCar> {
                   "Добавление авто",
                   style: TextStyle(fontSize: 34),
                 ),
-
-                Expanded(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          onChanged: (text) => {brand = text},
-                          autofocus: true,
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            labelText: "Марка авто",
-                            focusedBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.grey,
-                              ),
+                Flex(direction: Axis.horizontal, children: [
+                  Expanded(
+                      child: Column(
+                    children: [
+                      TextFormField(
+                        onChanged: (text) => {brand = text},
+                        autofocus: true,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: "Марка авто",
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
                             ),
                           ),
-                          validator: (value) {
-                            if (value?.length != 10) {
-                              return "Не больше 10 символов";
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          onChanged: (text) => {model = text},
-                          autofocus: true,
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            labelText: "Модель авто",
-                            focusedBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.grey,
-                              ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
                             ),
                           ),
-                          validator: (value) {
-                            if (value?.length != 10) {
-                              return "Не больше 10 символов";
-                            }
-                            return null;
-                          },
                         ),
-                        TextFormField(
-                          onChanged: (text) => {plateNumber = text},
-                          autofocus: true,
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            labelText: "Гос. Номер авто",
-                            focusedBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.grey,
-                              ),
+                        validator: (value) {
+                          if (value?.length != 10) {
+                            return "Не больше 10 символов";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        onChanged: (text) => {model = text},
+                        autofocus: true,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: "Модель авто",
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
                             ),
                           ),
-                          validator: (value) {
-                            if (value?.length != 8) {
-                              return "Не больше 8 символов";
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          onChanged: (text) => {vin = text},
-                          autofocus: true,
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            labelText: "VIN авто",
-                            focusedBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.grey,
-                              ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
                             ),
                           ),
-                          validator: (value) {
-                            if (value?.length != 12) {
-                              return "Не больше 12 символов";
-                            }
-                            return null;
-                          },
                         ),
-                        TextFormField(
-                          // onChanged: (text) => {year = text},
-                          autofocus: true,
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            labelText: "Год авто",
-                            focusedBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(
-                                color: Colors.grey,
-                              ),
+                        validator: (value) {
+                          if (value?.length != 10) {
+                            return "Не больше 10 символов";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        onChanged: (text) => {plateNumber = text},
+                        autofocus: true,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "Гос. Номер авто",
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
                             ),
                           ),
-                          validator: (value) {
-                            if (value?.length != 4) {
-                              return "Не больше 4 символов";
-                            }
-                            return null;
-                          },
+                          enabledBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
                         ),
-                      ],
-                    )
-                  ),
-
-
-
-
-
+                        validator: (value) {
+                          if (value?.length != 8) {
+                            return "Не больше 8 символов";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        onChanged: (text) => {vin = text},
+                        autofocus: true,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: "VIN авто",
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value?.length != 12) {
+                            return "Не больше 12 символов";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        onChanged: (text) => {year = int.parse(text)},
+                        autofocus: true,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: "Год авто",
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value?.length != 4) {
+                            return "Не больше 4 символов";
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  )),
+                ]),
                 ElevatedButton(
                   onPressed: () {
                     _createCar().then((response) {
@@ -199,8 +192,7 @@ class _CreateCarState  extends State<CreateCar> {
                       //       (route) => false,
                       //   // arguments: HomeArgs(initialState: 2),
                       // );
-                    }
-                    );
+                    });
                   },
                   child: const Text("Создать заказ"),
                 ),
@@ -220,15 +212,8 @@ class _CreateCarState  extends State<CreateCar> {
   }
 
   Future<int> _createCar() async {
-    final response = await createCar(
-      authToken,
-      brand,
-      model,
-      year ?? 0,
-      vin,
-      plateNumber
-
-    );
+    final response =
+        await createCar(authToken, brand, model, year ?? 0, vin, plateNumber);
     switch (response.statusCode) {
       case 200:
         {
@@ -243,7 +228,6 @@ class _CreateCarState  extends State<CreateCar> {
         }
       default:
         {
-
           debugPrint("Ошибка при создании Авто: ${response.statusCode}");
           debugPrint("response.error!.error=${response.error!.error}");
           debugPrint("response.error!.message=${response.error!.message}");
